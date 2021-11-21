ad6be397ae761ddc411a996fa3af3287d68115b4
Bryan Vasquez <bryan.f93@gmail.com>
2021-11-20 22:42:02 -0500

    edit from github

--- TestingFunctions/script.js
+++ TestingFunctions/script.js
@@ -7,4 +7,5 @@ console.log(sayHi.toString())
 // this does an implicit return. it returns the value without using return
 const addOneToOne = () => 1 + 1;
 
+// Log to the console
 console.log(addOneToOne())
